9f12f63ac6e444f8bbea84226aba08fc0f139be9
Aravind <47712052+Aravind1707@users.noreply.github.com>
2026-09-04 00:29:53 +0530

    Remove conflicting wallet migration number

--- db/migrations/015_remove_wallet.sql
+++
@@ -1,9 +0,0 @@
-USE genz_os;
-
-SET @has_wallet := (SELECT COUNT(*) FROM information_schema.columns WHERE table_schema=DATABASE() AND table_name='members' AND column_name='wallet_balance');
-SET @sql := IF(@has_wallet > 0, 'ALTER TABLE members DROP COLUMN wallet_balance', 'SELECT 1');
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-
-INSERT INTO schema_migrations(version,applied_at) VALUES(15,NOW(3)) ON DUPLICATE KEY UPDATE applied_at=applied_at;
